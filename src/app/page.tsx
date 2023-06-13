a07102bef986f10b109a9b336c95b4e644492a19
than2042 <than.than2042@gmail.com>
2023-06-13 18:51:37 +0100

    added login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,28 @@
-import Image from 'next/image'
+"use client";
 
+import { useState } from "react";
+import styles from "./page.module.css";
 
 export default function Home() {
+  const [userName, setUsername] = useState("");
+  const [user, setUser] = useState("");
+
+  console.log(user);
   return (
     <main>
-      <div >
-        
-        
-       <h1>Hello Notification</h1>
-       <p>first line</p>
+      <div className={styles.container}>
+        <div className={styles.login}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={() => setUser(userName)} className={styles.button}>
+            Login
+          </button>
+        </div>
       </div>
     </main>
-  )
+  );
 }
